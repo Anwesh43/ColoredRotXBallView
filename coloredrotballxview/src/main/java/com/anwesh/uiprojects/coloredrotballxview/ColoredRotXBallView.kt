c731194de2ca5290dc501c252ed49b16d3d79e40
Anwesh43 <anweshthecool0@gmail.com>
2020-05-28 04:57:33 +0530

    adding static  create method

--- coloredrotballxview/src/main/java/com/anwesh/uiprojects/coloredrotballxview/ColoredRotXBallView.kt
+++ coloredrotballxview/src/main/java/com/anwesh/uiprojects/coloredrotballxview/ColoredRotXBallView.kt
@@ -216,4 +216,13 @@ class ColoredRotXBallView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : ColoredRotXBallView {
+            val view : ColoredRotXBallView = ColoredRotXBallView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
